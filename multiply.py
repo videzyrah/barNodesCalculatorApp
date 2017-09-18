b0f83cd938a139822eccb0bd103edd38340d00fb
Greg Stull <greg.t.stull@my.sfcollege.edu>
2017-09-18 16:02:13 -0400

    chimes

--- multiply.py
+++ multiply.py
@@ -15,6 +15,24 @@ def success():
        print(factor1, factor2)
        return render_template("index.html", theSum= theSum)
 
+@app.route("/decInches", methods=['POST'])
+def decInches ():
+    if request.method=='POST':
+        f = request.form["intLength"]
+        s = request.form["fraxLength"]
+
+        #total length decimal inches
+        d = float(int(f) + int(s)/16)
+        #nodes in decimal inches
+        n = d * 0.224
+        #nodes integer portion
+        i = int(n)
+        #converting decimal portion to sixteenths of inches
+        r = round((n - i) * 16, 1)
+
+        message = "The nodes are located at " + str(i) + " and " + str(r) + " sixteenths of an inch from each end."
+        return render_template("index.html", message = message)
+
 if __name__ == '__main__':
     app.debug=True
     app.run()
